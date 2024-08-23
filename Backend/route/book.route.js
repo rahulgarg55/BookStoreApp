9d72c1cf88d8660215620a899d7fb67453f9dca1
rahulgarg55 <gargr010>
2024-08-23 21:05:40 +0530

    bookstoreapp

--- Backend/route/book.route.js
+++ Backend/route/book.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { getBook } from "../controller/book.controller.js";
 
-const router = express.Router();
+const router = express.Router();   // to create a new router object,
 
 router.get("/", getBook);
 
 export default router;
+
+//Using express.Router() helps in modularizing your Express application by creating separate router modules for different sets of routes. This improves the organization and maintainability of your code by allowing you to manage and mount routes in a structured way.
